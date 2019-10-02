4281f059db0f610f9ad210ebabfcff46a0469395
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-10-02 15:59:16 -0500

    get posts by user id done

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const server = express();
 
+
 //routes
 const userRoute = require('./users/userRouter')
+
+server.use(express.json())
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -14,7 +18,8 @@ server.use('/users', userRoute)
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`${req.method} to ${req.url} [${new Date().toISOString()}]`)
+  next()
 };
 
 const port = 8000
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,16 +1,17 @@
 const express = require('express');
 
+//databases
 const userDB = require('./userDb')
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    const newUser = req.body
 
-    userDB.insert(newUser)
-    .then(item => {
-        console.log(item)
+    userDB.insert(req.body)
+    .then(user => {
+        res.send(201).json(user)
     })
+    .catch(err => res.status(400))
 
 });
 
@@ -27,11 +28,23 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+    id = req.params.id
 
+    userDB.getById(id)
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => res.status(404))
 });
 
 router.get('/:id/posts', (req, res) => {
+    const id = req.params.id
 
+    userDB.getUserPosts(id)
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => res.status(404))
 });
 
 router.delete('/:id', (req, res) => {
